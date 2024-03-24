0c3a5a7102547f818b753d37bee21beedc601d36
odan <odan3240@gmail.com>
2024-03-24 13:51:08 +0900

    add tag

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import * as github from "@actions/github";
+
+const client = github.getOctokit(process.env.GITHUB_TOKEN);
+const resp = await client.rest.repos.getReleaseByTag({
+  owner: "odanado",
+  repo: "git-pr-release-go",
+  tag: "v0.0.3",
+});
+console.log(resp.data);
